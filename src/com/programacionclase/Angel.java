ead68e67d4fae5431ad1577277d870b4bc9d516e
Juan Vazquez Veloso <jvazquezveloso@gmail.com>
2021-02-05 10:12:43 +0100

    Merge hecho y programa finalizado

---
+++ src/com/programacionclase/Angel.java
@@ -0,0 +1,11 @@
+package com.programacionclase;
+
+/**
+ * Creado por @autor: angel
+ * El  05 de feb. de 2021.
+ **/
+public class Angel {
+    public void nombreCol(){
+        System.out.println("Angel");
+    }
+}
---
+++ src/com/programacionclase/Lider.java
@@ -0,0 +1,10 @@
+package com.programacionclase;
+
+public class Lider {
+    public void metodoLider(){
+        System.out.println("Creado el primer metodo.");
+    }
+    public void segundoMetodoLider(){
+        System.out.println("Creado el segundo metodo.");
+    }
+}
