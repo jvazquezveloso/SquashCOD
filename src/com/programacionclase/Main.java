2dcc1eb1e3a6b8fb9aee0c8e7309c07931935888
Juan Vazquez Veloso <jvazquezveloso@gmail.com>
2021-02-05 10:01:41 +0100

    Main preparado.

--- src/com/programacionclase/Main.java
+++ src/com/programacionclase/Main.java
@@ -3,6 +3,6 @@ package com.programacionclase;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Ejercicio para practicar squash.");
     }
 }
