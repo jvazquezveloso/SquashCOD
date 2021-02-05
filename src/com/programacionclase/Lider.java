bb5952e1305a9a788e32ea49e0308433dd8251be
Juan Vazquez Veloso <jvazquezveloso@gmail.com>
2021-02-05 10:05:23 +0100

    Creado el segundo método de la clase Lider y terminada la funcionalidad de la rama Lider.

--- src/com/programacionclase/Lider.java
+++ src/com/programacionclase/Lider.java
@@ -4,4 +4,7 @@ public class Lider {
     public void metodoLider(){
         System.out.println("Creado el primer metodo.");
     }
+    public void segundoMetodoLider(){
+        System.out.println("Creado el segundo metodo.");
+    }
 }
